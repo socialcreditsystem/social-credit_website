b05b787926d78afe9527c9e97d9e94532ec6bfa9
Philipp Danne <philipp@bandship.com>
2019-01-02 18:25:20 +0100

    Driite Sektion implementiert

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,7 +2,7 @@
     <section class="intro">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-7 intro__text-wrapper">
+                <div class="col-lg-7 col-12 intro__text-wrapper">
                     <h2 class="section__heading"><span class="section__heading--bold">Social Credit System</span><span class="section__heading--light"> What?</span></h2>
                     <p class="section__paragraph">In recent years, China has made rapid advances in the incorporation of big data technologies in governance processes. In addition to using big data for more efficient policy making and enforcement, cutting-edge technologies are increasingly being used for social control and social management in order to address societal problems and pre-empt social instabilities. The emerging Social Credit System (SCS) stands out as an initiative with the potential to radically transform the state’s governance of both society and the economy. Under this system, individuals, businesses, social organisations and government agencies are assessed based on their ‘trustworthiness’.</p>
                     <img class="section__image" src="https://picsum.photos/1400/1000" alt="Chinese people being rated by a camera software (symbolic picture)">
@@ -17,6 +17,6 @@
 import "../style/intro/intro.scss";
 
 export default {
-  name: "Stage"
+  name: "Intro"
 };
 </script>
---
+++ src/components/Types.vue
@@ -0,0 +1,74 @@
+<template>
+    <section class="types">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-8 types__intro">
+                    <h2 class="section__heading"><span class="section__heading--light">SCS</span><span class="section__heading--bold"> Types</span></h2>
+                    <p class="section__paragraph">To really understand the Social Credit System you should know that there are two types of SCS - commercial and governmental ones.</p>
+                </div>
+            </div>
+            <div class="row justify-content-center types__wrapper">
+                <div class="col-6 types__element types__element--commercial-scs">
+                    <h2 class="types__element__heading"><span class="section__heading--bold">Commercial</span><span class="section__heading--light"> SCS</span></h2>
+                    <ul class="types__element__list">
+                        <li>Eight different SCSs by Chinese companies e. g. Sesame Credit (Alibaba Group) and Tencent Credit</li>
+                        <li>Free decision of partcipation</li>
+                    </ul>
+                    <div class="types__element__approval">
+                        <h4 class="types__element__subheading">Approval among participants</h4>
+                        <div class="types__element__approval__bar"><div class="types__element__approval__bar-fill"></div></div>
+                        <div class="types__element__approval__number">55%</div>
+                    </div>
+                    <div class="types__element__usage">
+                        <h4 class="types__element__subheading">Usage</h4>
+                        <p class="types__element__usage__text">4 out of 5 people in China take part in a commercial SCS.</p>
+
+                        <div class="types__element__usage__visualization row justify-content-center">
+                            <div class="col-2 types__element__usage__visualization-element p1"></div>
+                            <div class="col-2 types__element__usage__visualization-element p2"></div>
+                            <div class="col-2 types__element__usage__visualization-element p3"></div>
+                            <div class="col-2 types__element__usage__visualization-element p4"></div>
+                            <div class="col-2 types__element__usage__visualization-element p5"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 types__element types__element--governmental-scs">
+                    <h2 class="types__element__heading"><span class="section__heading--bold">Governmental</span><span class="section__heading--light"> SCS</span></h2>
+                    <ul class="types__element__list">
+                        <li>40 different SCSs by the government & it’s municipals and provinces</li>
+                        <li>Some model cities with more strongly developed SCS e. g. Rongcheng and Weihai</li>
+                        <li>Mandatory participation</li>
+                    </ul>
+                    <div class="types__element__approval">
+                        <h4 class="types__element__subheading">Approval among participants</h4>
+                        <div class="types__element__approval__bar"><div class="types__element__approval__bar-fill"></div></div>
+                        <div class="types__element__approval__number">64%</div>
+                    </div>
+                    <div class="types__element__usage">
+                        <h4 class="types__element__subheading">Usage</h4>
+                        <p class="types__element__usage__text">Approximately 2 out of 5 people in China take part in a governmental SCS.</p>
+
+                        <div class="types__element__usage__visualization row justify-content-center">
+                            <div class="col-2 types__element__usage__visualization-element p1"></div>
+                            <div class="col-2 types__element__usage__visualization-element p2"></div>
+                            <div class="col-2 types__element__usage__visualization-element p3"></div>
+                            <div class="col-2 types__element__usage__visualization-element p4"></div>
+                            <div class="col-2 types__element__usage__visualization-element p5"></div>
+                        </div>
+
+                        <p class="types__element__usage__text">Only 11% of these affected citizens know that they take part.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+// Stylesheets
+import "../style/types/types.scss";
+
+export default {
+  name: "Types"
+};
+</script>
