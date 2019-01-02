82ad9f5df1e9c7cb093b6e7da177f7ced1bb3686
Philipp Danne <philipp@bandship.com>
2019-01-02 15:22:08 +0100

    Hintergrundbild in erster Sektion hinzugefügt

--- src/components/stage.vue
+++ src/components/stage.vue
@@ -4,7 +4,7 @@
         <div class="row justify-content-center">
           <div class="col-9 stage__headline">
             <h3>Seen through Chinese eyes</h3>
-            <h1>China's<br/>Social Credit System</h1>
+            <h1>China’s<br/>Social Credit System</h1>
             <h6>A visualization by the students Benedikt Asal, Philipp Danne, Selina Groß and David Wostracky of the Online Media course of studies at Baden-Württemberg Cooperative State University Mosbach.</h6>
           </div>
         </div>
