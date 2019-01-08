ce7975376df59ac8e5b182a9a1942e22ebdd0914
davidwsty <dav.wostracky.16@lehre.mosbach.dhbw.de>
2019-01-08 22:59:24 +0100

    AmCharts zu Sektion Rural vs. Urban hinzugefügt + Navigation angepasst

--- src/components/RualUrban.vue
+++ src/components/RualUrban.vue
@@ -35,8 +35,8 @@
         <div class="col-4">
           <p class="section__paragraph">Obtained credit without difficulties</p>
         </div>
-        <div class="col-4">
-          <p class="section__paragraph">Diagramm</p>
+        <div class="col-5">
+          <div class="ruralurban0 ruralurban__diagramm" ref="ruralurban0"></div>
         </div>
       </div>
 
@@ -44,8 +44,8 @@
         <div class="col-4">
           <p class="section__paragraph">Obtained credit with difficulties</p>
         </div>
-        <div class="col-4">
-          <p class="section__paragraph">Diagramm</p>
+        <div class="col-5">
+          <div class="ruralurban1 ruralurban__diagramm" ref="ruralurban1"></div>
         </div>
       </div>
 
@@ -53,8 +53,8 @@
         <div class="col-4">
           <p class="section__paragraph">Received lower interest rates on loans from my bank</p>
         </div>
-        <div class="col-4">
-          <p class="section__paragraph">Diagramm</p>
+        <div class="col-5">
+          <div class="ruralurban2 ruralurban__diagramm" ref="ruralurban2"></div>
         </div>
       </div>
 
@@ -62,8 +62,8 @@
         <div class="col-4">
           <p class="section__paragraph">Received higher interest rates on savings from my bank</p>
         </div>
-        <div class="col-4">
-          <p class="section__paragraph">Diagramm</p>
+        <div class="col-5">
+          <div class="ruralurban3 ruralurban__diagramm" ref="ruralurban3"></div>
         </div>
       </div>
 
@@ -71,8 +71,8 @@
         <div class="col-4">
           <p class="section__paragraph">Received a fast-tracked visa</p>
         </div>
-        <div class="col-4">
-          <p class="section__paragraph">Diagramm</p>
+        <div class="col-5">
+          <div class="ruralurban4 ruralurban__diagramm" ref="ruralurban4"></div>
         </div>
       </div>
 
@@ -82,8 +82,8 @@
             class="section__paragraph"
           >Used deposit-free sharing economy services (such as a rental bike or car)</p>
         </div>
-        <div class="col-4">
-          <p class="section__paragraph">Diagramm</p>
+        <div class="col-5">
+          <div class="ruralurban5 ruralurban__diagramm" ref="ruralurban5"></div>
         </div>
       </div>
 
@@ -91,8 +91,8 @@
         <div class="col-4">
           <p class="section__paragraph">Used fast-tracked check-ins for hotels or flights</p>
         </div>
-        <div class="col-4">
-          <p class="section__paragraph">Diagramm</p>
+        <div class="col-5">
+          <div class="ruralurban6 ruralurban__diagramm" ref="ruralurban6"></div>
         </div>
       </div>
 
@@ -100,8 +100,8 @@
         <div class="col-4">
           <p class="section__paragraph">Experienced a positive impact on my online dating</p>
         </div>
-        <div class="col-4">
-          <p class="section__paragraph">Diagramm</p>
+        <div class="col-5">
+          <div class="ruralurban7 ruralurban__diagramm" ref="ruralurban7"></div>
         </div>
       </div>
     </div>
@@ -111,7 +111,72 @@
 <script>
 import "../style/rualurban/rualurban.scss";
 
+/* Imports */
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
+// Themes end
+
 export default {
-  name: "RualUrban"
+  name: "RualUrban",
+  mounted() {
+    let data_urban = ["37", "2", "18", "11", "14", "40", "35", "4"];
+    let data_rural = ["31", "5", "12", "9", "11", "32", "28", "4"];
+
+    for (let i = 0; i < 8; i++) {
+      // Create chart instance
+      let chart = am4core.create("ruralurban" + i, am4charts.XYChart);
+
+      // Add data
+      chart.data = [
+        {
+          location: "Urban",
+          percentage: data_urban[i],
+          color: am4core.color("#D5433D")
+        },
+        {
+          location: "Rural",
+          percentage: data_rural[i],
+          color: am4core.color("#3B4C81")
+        }
+      ];
+
+      // Create axes
+      let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = "location";
+      categoryAxis.renderer.grid.template.disabled = true;
+      categoryAxis.renderer.minGridDistance = 30;
+      categoryAxis.renderer.labels.template.disabled = true;
+      categoryAxis.renderer.labels.template.fontSize = 20;
+
+      let valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+      valueAxis.renderer.grid.template.strokeDasharray = "4,4";
+      valueAxis.renderer.labels.template.disabled = true;
+      valueAxis.min = 0;
+      valueAxis.max = 40;
+
+      // Do not crop bullets
+      chart.maskBullets = false;
+      // Remove padding
+      chart.paddingBottom = 0;
+
+      // Create series
+      let series = chart.series.push(new am4charts.ColumnSeries());
+      series.dataFields.valueX = "percentage";
+      series.dataFields.categoryY = "location";
+      series.columns.template.propertyFields.fill = "color";
+      series.columns.template.propertyFields.stroke = "color";
+      series.columns.template.tooltipText = "{categoryY}: [bold]{valueX}%[/b]";
+
+      // Create Percentage
+      let percentageLabel = series.bullets.push(new am4charts.LabelBullet());
+      percentageLabel.label.text = "{valueX}%";
+
+      percentageLabel.label.dx = 30;
+      percentageLabel.label.fill = am4core.color("#fff");
+    }
+  }
 };
 </script>
