f362920465d0df0f658295a9fee1f99bc70969aa
davidwsty <dav.wostracky.16@lehre.mosbach.dhbw.de>
2019-01-11 15:47:42 +0100

    Anpassung Design Impressum

--- src/components/Imprint.vue
+++ src/components/Imprint.vue
@@ -23,18 +23,26 @@
           <p
             class="section__paragraph"
           >Visualized by the students Benedikt Asal, Philipp Danne, Selina Groß and David Wostracky</p>
-          <p
-            class="section__paragraph"
-          >Based on the study China’s Social Credit Systems and Public Opinion: Explaining High Levels of Approval by Prof. Dr. Genia Kostka (2018)</p>
+          <p class="section__paragraph">
+            Based on the study
+            <a
+              class="section__paragraph__link"
+              href="https://www.researchgate.net/publication/326625329_China%27s_Social_Credit_Systems_and_Public_Opinion_Explaining_High_Levels_of_Approval"
+              target="blank"
+            >China’s Social Credit Systems and Public Opinion: Explaining High Levels of Approval</a> by Prof. Dr. Genia Kostka (2018)
+          </p>
 
           <p class="section__paragraph">
             We have used icons by Freepik and OCHA from
             <a
               class="section__paragraph__link"
-              href="www.flaticon.com"
+              href="https://www.flaticon.com/"
               target="blank"
             >www.flaticon.com</a>
           </p>
+          <div class="section__imprint__logo">
+            <img width="184px" src="../assets/imgs/DHBW_d_MOS_46mm_4c_ohne_schutzzone.jpg">
+          </div>
         </div>
       </div>
     </div>
