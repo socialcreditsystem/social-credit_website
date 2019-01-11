b4e07556a2268cbf041cd8b2bfc3e07f49b61c4f
davidwsty <dav.wostracky.16@lehre.mosbach.dhbw.de>
2019-01-11 15:08:40 +0100

    Parallax Sektion angepasst + Navigation

--- src/components/Country.vue
+++ src/components/Country.vue
@@ -18,12 +18,12 @@
     <div class="parallaxcountry">
       <div class="parallax--right">
         <p
-          class="parallax__paragraph"
+          class="parallax__paragraph paragraph--light"
         >The findings show, further, that there is less approval for SCSs in rural areas. One potential explanation is that respondents in rural areas are less familiar with SCSs and hence more sceptical.</p>
       </div>
       <div class="parallax--left">
         <p
-          class="parallax__paragraph"
+          class="parallax__paragraph paragraph--light"
         >For example, 43% citizens from the countryside report that they don't know how their score is calculated. But 87% have already received benefits by participating in a commercial SCS pilot.</p>
       </div>
     </div>
@@ -31,14 +31,14 @@
     <div class="parallaxcity">
       <div class="parallax--right">
         <p
-          class="parallax__paragraph"
+          class="parallax__paragraph paragraph--dark"
         >In contrast to that, the knowledge about how the own score is calculated is higher for citizens from the urban area. Only 36% do not know how the score is calculated. This is 7% less than in rural areas.</p>
       </div>
     </div>
     <div class="parallaxcity">
       <div class="parallax--left">
         <p
-          class="parallax__paragraph"
+          class="parallax__paragraph paragraph--dark"
         >The number of people who have already received benefits from participating in a commercial SCS is almost the same in the city. Only one percent more report that they have received benefits (88%).</p>
       </div>
     </div>
