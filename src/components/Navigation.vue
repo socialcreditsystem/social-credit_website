4a00a88fa9d9b670edf8297d38eaefd985482638
davidwsty <dav.wostracky.16@lehre.mosbach.dhbw.de>
2019-01-18 11:04:13 +0100

    Parallax Sektion angepasst + Navigation + DHBW Logo positioniert
    Header wie im Design platziert
    Texte und Grafik formatiert

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,32 +4,52 @@
       <div class="row justify-content-center">
         <div class="col-9 navigation">
           <ul class="list-unstyled">
-            <li>
-              <a class="navigation_button" href="#" data-toggle="collapse" data-target="#Menu">
-                <img class="navigation-button-picture" src="../assets/navigation-plus.svg">
+            <li class="navigation__main__list">
+              <a class="navigation__button" href="#" data-toggle="collapse" data-target="#menu">
+                <img
+                  style="padding-top: 20px;"
+                  class="navigation__button-picture"
+                  src="../assets/navigation-plus.svg"
+                >
               </a>
-              <div class="collapse in" id="Menu">
-                <div class="triangle"></div>
-                <ul class="list-unstyled navigation_list">
-                  <a class="navigation_element intro-nav" href="#intro">Introduction</a>
-
+              <div class="collapse in collapsed__navigation__menu" id="menu">
+                <a class="navigation__button" href="#" data-toggle="collapse" data-target="#menu">
+                  <img
+                    class="navigation__button-picture__inline"
+                    style="padding-top: 20px;"
+                    src="../assets/navigation-plus.svg"
+                  >
+                </a>
+                <ul class="list-unstyled navigation__list">
                   <li>
-                    <a class="navigation_element types-nav" href="#types">SCS-Types</a>
+                    <a class="navigation__element intro-nav" href="#intro">Introduction</a>
                   </li>
                   <li>
-                    <a class="navigation_element behavior-nav" href="#behavior">Behavior</a>
+                    <a class="navigation__element types-nav" href="#types">SCS Types</a>
                   </li>
                   <li>
-                    <a class="navigation_element usage-nav" href="#dissemination">Usage</a>
+                    <a class="navigation__element behavior-nav" href="#behavior">Behavior</a>
                   </li>
                   <li>
-                    <a class="navigation_element approval-nav" href="#approval">Approval</a>
+                    <a class="navigation__element usage-nav" href="#dissemination">Usage</a>
+                  </li>
+                  <li>
+                    <a class="navigation__element approval-nav" href="#approval">Approval</a>
                   </li>
                   <li>
                     <a
-                      class="navigation_element cityvscountry-nav"
+                      class="navigation__element cityvscountry-nav"
                       href="#cityvscountry"
-                    >City vs. Country</a>
+                    >City / Country</a>
+                  </li>
+                  <li>
+                    <a
+                      class="navigation__element perspectives-nav"
+                      href="#peoples-perspective"
+                    >Perspectives</a>
+                  </li>
+                  <li>
+                    <a class="navigation__element conclusion-nav" href="#conclusion">Conclusion</a>
                   </li>
                 </ul>
               </div>
@@ -50,29 +70,37 @@ $(document).ready(function() {
   var top5 = $("#dissemination").offset().top - 1;
   var top6 = $("#approval").offset().top - 1;
   var top7 = $("#cityvscountry").offset().top - 1;
+  var top8 = $("#peoples-perspective").offset().top - 1;
+  var top9 = $("#conclusion").offset().top - 1;
 
   $(document).scroll(function() {
     var scrollPos = $(document).scrollTop();
     if (scrollPos >= top1 && scrollPos < top2) {
-      $(".navigation_element").removeClass("nav-active");
+      $(".navigation__element").removeClass("nav-active");
     } else if (scrollPos >= top2 && scrollPos < top3) {
-      $(".navigation_element").removeClass("nav-active");
+      $(".navigation__element").removeClass("nav-active");
       $(".intro-nav").addClass("nav-active");
     } else if (scrollPos >= top3 && scrollPos < top4) {
-      $(".navigation_element").removeClass("nav-active");
+      $(".navigation__element").removeClass("nav-active");
       $(".types-nav").addClass("nav-active");
     } else if (scrollPos >= top4 && scrollPos < top5) {
-      $(".navigation_element").removeClass("nav-active");
+      $(".navigation__element").removeClass("nav-active");
       $(".behavior-nav").addClass("nav-active");
     } else if (scrollPos >= top5 && scrollPos < top6) {
-      $(".navigation_element").removeClass("nav-active");
+      $(".navigation__element").removeClass("nav-active");
       $(".usage-nav").addClass("nav-active");
     } else if (scrollPos >= top6 && scrollPos < top7) {
-      $(".navigation_element").removeClass("nav-active");
+      $(".navigation__element").removeClass("nav-active");
       $(".approval-nav").addClass("nav-active");
-    } else if (scrollPos >= top7) {
-      $(".navigation_element").removeClass("nav-active");
+    } else if (scrollPos >= top7 && scrollPos < top8) {
+      $(".navigation__element").removeClass("nav-active");
       $(".cityvscountry-nav").addClass("nav-active");
+    } else if (scrollPos >= top8 && scrollPos < top9) {
+      $(".navigation__element").removeClass("nav-active");
+      $(".perspectives-nav").addClass("nav-active");
+    } else if (scrollPos >= top9) {
+      $(".navigation__element").removeClass("nav-active");
+      $(".conclusion-nav").addClass("nav-active");
     }
   });
 });
