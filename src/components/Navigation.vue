e7322406f1a9c742b5af6d5f9344325e26b67235
davidwsty <dav.wostracky.16@lehre.mosbach.dhbw.de>
2019-01-18 18:48:15 +0100

    Navigation Icons geändert

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,21 +3,22 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-9 navigation">
+          <a class="navigation__button" href="#" data-toggle="collapse" data-target="#menu">
+            <img
+              style="padding-top: 20px;"
+              class="navigation__button-picture"
+              src="../assets/navButtonCircle.svg"
+            >
+          </a>
+
           <ul class="list-unstyled">
             <li class="navigation__main__list">
-              <a class="navigation__button" href="#" data-toggle="collapse" data-target="#menu">
-                <img
-                  style="padding-top: 20px;"
-                  class="navigation__button-picture"
-                  src="../assets/navigation-plus.svg"
-                >
-              </a>
               <div class="collapse in collapsed__navigation__menu" id="menu">
                 <a class="navigation__button" href="#" data-toggle="collapse" data-target="#menu">
                   <img
                     class="navigation__button-picture__inline"
                     style="padding-top: 20px;"
-                    src="../assets/navigation-plus.svg"
+                    src="../assets/navOpen.svg"
                   >
                 </a>
                 <ul class="list-unstyled navigation__list">
