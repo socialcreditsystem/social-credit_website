123dd7b101893ffb0b2a67e30d2468ed1707679c
Philipp Danne <philipp@bandship.com>
2019-01-20 19:15:50 +0100

    Kleine Designverbesserungen

--- src/components/PeoplesPerspective.vue
+++ src/components/PeoplesPerspective.vue
@@ -1,87 +1,92 @@
 <template>
   <section class="peoples-perspective" id="peoples-perspective">
-      <h2 class="section__heading">
-            <span class="section__heading--bold">People's </span>
-            <span class="section__heading--light">Perspective</span>
-        </h2>
-      <div class="peoples-perspective__overlay"></div>
-      <table>
+      <div class="container">
+        <div class="row justify-content-center peoples-perspective__intro">
+            <div class="col-6">
+                <h2 class="section__heading">
+                    <span class="section__heading--bold">People's </span>
+                    <span class="section__heading--light">Perspective</span>
+                </h2>
+                <p class="section__paragraph">We have already seen that the Chinese are very positive about the SCS. The best way to understand why this is the case is to let the citizens speak.</p>
+            </div>
+        </div>
+      </div>
+      <table class="peoples-perspective__table">
+        <div class="peoples-perspective__overlay peoples-perspective__overlay--horizontal"></div>
+        <div class="peoples-perspective__overlay peoples-perspective__overlay--vertical"></div>
         <tbody>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td colspan=8></td>
+                <td><img src="https://picsum.photos/300/300"></td>
             </tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td colspan=4 rowspan=4>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td colspan=6></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
             </tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
             </tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td colspan=4 rowspan=4 id="qouteContent" class="peoples-perspective__table__content"><p class="peoples-perspective__table__content__paragraph">„Take, for instance, the example of using shared bikes.
+If someone does not lock a shared bike after using it properly, her or his own credit will be influenced. Alipay can collect such very detailed information from different aspects in life and include this in a score. Through such detailed accounting, SCSs can track individuals’ actions and create trust in society“</p></td>
+                <td><img src="https://picsum.photos/300/300"></td>
             </tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
             </tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
             </tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+            </tr>
+            <tr>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+            </tr>
+            <tr class="peoples-perspective__table--last-row">
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
+                <td><img src="https://picsum.photos/300/300"></td>
             </tr>
         </tbody>
         </table>
