a3628bf1bfea2dff95b0ebfb7f56fdfdda84d3f3
davidwsty <dav.wostracky.16@lehre.mosbach.dhbw.de>
2019-01-25 16:39:48 +0100

    Diverse Design Apnassungen mit Benni

--- src/components/Dissemination.vue
+++ src/components/Dissemination.vue
@@ -29,113 +29,136 @@ import "../style/dissemination/dissemination.scss";
 export default {
   name: "Dissemination",
   mounted() {
-    let chart = am4core.create("disseminationChart", am4charts.RadarChart);
+    let loaded = false;
 
-    // Add data
-    chart.data = [
-      {
-        category: "Government PilotResearch",
-        value: 7.4,
-        full: 100
-      },
-      {
-        category: "I don't know",
-        value: 8.18,
-        full: 100
-      },
-      {
-        category: "I do not take part in any social credit system",
-        value: 16.47,
-        full: 100
-      },
-      {
-        category: "Tencent Credit",
-        value: 30.6,
-        full: 100
-      },
-      {
-        category: "Sesame Credit",
-        value: 58.18,
-        full: 100
-      },
-      {
-        category: "Commercial Credit System",
-        value: 80.04,
-        full: 100
+    // set scroll events
+    jQuery(document).on("scroll load touchmove", handleScroll);
+    jQuery(window).on("load", handleScroll);
+
+    function handleScroll() {
+      var $ = jQuery;
+      if (true === loaded) return;
+      var hT = $("#dissemination").offset().top,
+        hH = $("#dissemination").outerHeight() / 2,
+        wH = $(window).height(),
+        wS = $(window).scrollTop();
+      if (wS > hT + hH - wH) {
+        loaded = true;
+        let chart = am4core.create("disseminationChart", am4charts.RadarChart);
+        // Add data
+
+        chart.data = [
+          {
+            category: "Government PilotResearch",
+            value: 7.4,
+            full: 100
+          },
+          {
+            category: "I don't know",
+            value: 8.18,
+            full: 100
+          },
+          {
+            category: "I do not take part in any social credit system",
+            value: 16.47,
+            full: 100
+          },
+          {
+            category: "Tencent Credit",
+            value: 30.6,
+            full: 100
+          },
+          {
+            category: "Sesame Credit",
+            value: 58.18,
+            full: 100
+          },
+          {
+            category: "Commercial Credit System",
+            value: 80.04,
+            full: 100
+          }
+        ];
+
+        // Make chart not full circle
+        chart.startAngle = -90;
+        chart.endAngle = 180;
+        chart.innerRadius = am4core.percent(20);
+
+        // Set number format
+        chart.numberFormatter.numberFormat = "#.#'%'";
+
+        // Create axes
+        let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "category";
+
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.grid.template.strokeOpacity = 0;
+        categoryAxis.renderer.labels.template.horizontalCenter = "right";
+        categoryAxis.renderer.labels.template.fontWeight = 500;
+        categoryAxis.renderer.labels.template.adapter.add("fill", function(
+          fill,
+          target
+        ) {
+          return target.dataItem.index >= 0
+            ? chart.colors.getIndex(target.dataItem.index)
+            : fill;
+        });
+        categoryAxis.renderer.minGridDistance = 10;
+
+        let valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+        valueAxis.renderer.grid.template.strokeOpacity = 0;
+
+        valueAxis.min = 0;
+        valueAxis.max = 100;
+        valueAxis.strictMinMax = true;
+        // Prozentangaben
+        valueAxis.renderer.labels.template.fill = am4core.color("#ffffff");
+
+        chart.colors.list = [
+          am4core.color("#3B4C81").lighten(0.2),
+          am4core.color("#757575"),
+          am4core.color("#757575").lighten(0.2),
+          am4core.color("#EF5440").lighten(-0.16),
+          am4core.color("#EF5440").lighten(-0.08),
+          am4core.color("#EF5440")
+        ];
+
+        // Create series
+        let series1 = chart.series.push(new am4charts.RadarColumnSeries());
+        series1.dataFields.valueX = "full";
+        series1.dataFields.categoryY = "category";
+        series1.clustered = false;
+        series1.columns.template.fill = new am4core.InterfaceColorSet().getFor(
+          "alternativeBackground"
+        );
+        series1.columns.template.fillOpacity = 0.08;
+        series1.columns.template.cornerRadiusTopLeft = 20;
+        series1.columns.template.strokeWidth = 0;
+        series1.columns.template.radarColumn.cornerRadius = 20;
+
+        let series2 = chart.series.push(new am4charts.RadarColumnSeries());
+        series2.dataFields.valueX = "value";
+        series2.dataFields.categoryY = "category";
+        series2.clustered = false;
+        series2.columns.template.strokeWidth = 0;
+        series2.columns.template.tooltipText = "{category}: [bold]{value}[/]";
+        series2.columns.template.radarColumn.cornerRadius = 20;
+
+        series2.sequencedInterpolation = false;
+        series2.defaultState.transitionDuration = 2500;
+
+        series2.columns.template.adapter.add("fill", function(fill, target) {
+          return chart.colors.getIndex(target.dataItem.index);
+        });
+        return;
       }
-    ];
+    }
 
-    // Make chart not full circle
-    chart.startAngle = -90;
-    chart.endAngle = 180;
-    chart.innerRadius = am4core.percent(20);
-
-    // Set number format
-    chart.numberFormatter.numberFormat = "#.#'%'";
-
-    // Create axes
-    let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-    categoryAxis.dataFields.category = "category";
-
-    categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.renderer.grid.template.strokeOpacity = 0;
-    categoryAxis.renderer.labels.template.horizontalCenter = "right";
-    categoryAxis.renderer.labels.template.fontWeight = 500;
-    categoryAxis.renderer.labels.template.adapter.add("fill", function(
-      fill,
-      target
-    ) {
-      return target.dataItem.index >= 0
-        ? chart.colors.getIndex(target.dataItem.index)
-        : fill;
-    });
-    categoryAxis.renderer.minGridDistance = 10;
-
-    let valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-    valueAxis.renderer.grid.template.strokeOpacity = 0;
-
-    valueAxis.min = 0;
-    valueAxis.max = 100;
-    valueAxis.strictMinMax = true;
-    // Prozentangaben
-    valueAxis.renderer.labels.template.fill = am4core.color("#ffffff");
-
-    chart.colors.list = [
-      am4core.color("#3B4C81").lighten(0.2),
-      am4core.color("#757575"),
-      am4core.color("#757575").lighten(0.2),
-      am4core.color("#EF5440").lighten(-0.16),
-      am4core.color("#EF5440").lighten(-0.08),
-      am4core.color("#EF5440")
-    ];
-
-    // Create series
-    let series1 = chart.series.push(new am4charts.RadarColumnSeries());
-    series1.dataFields.valueX = "full";
-    series1.dataFields.categoryY = "category";
-    series1.clustered = false;
-    series1.columns.template.fill = new am4core.InterfaceColorSet().getFor(
-      "alternativeBackground"
-    );
-    series1.columns.template.fillOpacity = 0.08;
-    series1.columns.template.cornerRadiusTopLeft = 20;
-    series1.columns.template.strokeWidth = 0;
-    series1.columns.template.radarColumn.cornerRadius = 20;
-
-    let series2 = chart.series.push(new am4charts.RadarColumnSeries());
-    series2.dataFields.valueX = "value";
-    series2.dataFields.categoryY = "category";
-    series2.clustered = false;
-    series2.columns.template.strokeWidth = 0;
-    series2.columns.template.tooltipText = "{category}: [bold]{value}[/]";
-    series2.columns.template.radarColumn.cornerRadius = 20;
-
-    series2.sequencedInterpolation = false;
-    series2.defaultState.transitionDuration = 2500;
-
-    series2.columns.template.adapter.add("fill", function(fill, target) {
-      return chart.colors.getIndex(target.dataItem.index);
-    });
+    // Return fake listener to avoid errors
+    return {
+      addListener: function() {}
+    };
   }
 };
 </script>
--- src/components/Source.vue
+++ src/components/Source.vue
@@ -11,15 +11,15 @@
             <span class="section__heading--light">&nbsp;source</span>
           </h2>
           <p
-            class="section__paragraph"
+            class="section__paragraph section__data__paragraph"
           >We have used the study "China's Social Credit Systems and Public Opinion: Explaining High Levels of Approval" by Prof. Dr. Genia Kostka to create this visualization.</p>
-          <p class="section__paragraph">
+          <p class="section__paragraph section__data__paragraph">
             The data has been retreived with an
             <b>online survey</b> conducted between February and April 2018 with 2,209 respondents across China. Additionally, fifteen
             <b>semi-structured interviews</b> were conducted with SCS users during March and July 2018 in Beijing and Shanghai.
           </p>
 
-          <p class="section__paragraph">
+          <p class="section__paragraph section__data__paragraph">
             The study tries to investigate the following aspects:
             <br>1. Document the overall
             <b>level of citizens’ (non-)approval</b> of different SCSs
@@ -28,13 +28,13 @@
           </p>
 
           <p
-            class="section__paragraph"
+            class="section__paragraph section__data__paragraph"
           >In the study, the participants were informed that the data would be anonymised. Nevertheless, the question arises as to whether the participants actually expressed their personal opinions. The effect on the studys results is considered marginal by the author.</p>
 
-          <p class="section__paragraph">
+          <p class="section__paragraph section__data__paragraph">
             The whole study can be read on
             <a
-              class="section__paragraph__link"
+              class="section__paragraph__link section__data__paragraph"
               href="https://www.researchgate.net/publication/326625329_China%27s_Social_Credit_Systems_and_Public_Opinion_Explaining_High_Levels_of_Approval"
               target="blank"
             >ResearchGate</a>.
