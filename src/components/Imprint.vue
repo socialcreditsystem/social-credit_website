3aec62d76e2366de0ea6c23892a8b80aedbbf253
davidwsty <dav.wostracky.16@lehre.mosbach.dhbw.de>
2019-01-26 16:55:26 +0100

    Design Änderungen von Benni und Selina

--- src/components/Imprint.vue
+++ src/components/Imprint.vue
@@ -13,36 +13,39 @@
           <h1>China’s
             <br>Social Credit System
           </h1>
-          <h3>Seen through Chinese eyes</h3>
           <div class="section__imprint__placeholder"></div>
           <div class="section__imprint__logo">
-            <img width="184px" src="../assets/imgs/DHBW_d_MOS_46mm_4c_ohne_schutzzone.jpg">
+            <img width="164px" src="../assets/imgs/DHBW_d_MOS_46mm_4c_ohne_schutzzone.jpg">
           </div>
         </div>
         <div class="col-6 imprint__text-wrapper">
-          <p class="section__paragraph text-left">
+          <p class="section__paragraph text-left section__paragraph_imprint">
             A semester paper in the course Webdesign 3 - Interactive Information Design
             Lecturer Dipl.-Des. Lothar B. Blum
           </p>
           <p
-            class="section__paragraph text-left"
+            class="section__paragraph text-left section__paragraph_imprint"
           >Visualized by the students Benedikt Asal, Philipp Danne, Selina Groß and David Wostracky</p>
-          <p class="section__paragraph text-left">DHBW Campus Mosbach, Department Online Media</p>
+          <p
+            class="section__paragraph text-left section__paragraph_imprint"
+          >DHBW Campus Mosbach, Department Online Media</p>
 
-          <p class="section__paragraph text-left">Date of submission: 28.01.2019</p>
-          <p class="section__paragraph text-left">
+          <p
+            class="section__paragraph text-left section__paragraph_imprint"
+          >Date of submission: 28.01.2019</p>
+          <p class="section__paragraph text-left section__paragraph_imprint">
             Based on the study
             <a
-              class="section__paragraph__link"
+              class="section__paragraph__link section__paragraph_imprint"
               href="https://www.researchgate.net/publication/326625329_China%27s_Social_Credit_Systems_and_Public_Opinion_Explaining_High_Levels_of_Approval"
               target="blank"
             >China’s Social Credit Systems and Public Opinion: Explaining High Levels of Approval</a> by Prof. Dr. Genia Kostka (2018)
           </p>
 
-          <p class="section__paragraph text-left">
+          <p class="section__paragraph text-left section__paragraph_imprintt">
             We have used icons by Freepik and OCHA from
             <a
-              class="section__paragraph__link"
+              class="section__paragraph__link section__paragraph_imprint"
               href="https://www.flaticon.com/"
               target="blank"
             >www.flaticon.com</a>
